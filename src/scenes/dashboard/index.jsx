02d28a8c6f9882787ec81a45f9bf0a8ade75b006
Muhangi2 <eliodamutiba@gmail.com>
2024-07-01 12:15:36 -0700

    latest

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-import { useGetCategoryQuery, useGetBusinessQuery, useGetSuppliersQuery, useGetClientsQuery, useGetContractorsQuery } from 'state/api'
+import {useGetBusinessQuery, useGetSuppliersQuery, useGetClientsQuery, useGetContractorsQuery,useGetCategoryQuery,useGetConsutantsQuery,useGetSuppliertypesQuery,useGetDealsQuery } from 'state/api'
 import Header from 'components/Header'
 import FlexBetween from 'components/FlexBetween'
-import { AttachMoneyOutlined, Groups2Outlined, ReceiptLongOutlined, ShoppingCartOutlined } from '@mui/icons-material'
+import { AttachMoneyOutlined,PointOfSaleOutlined,Groups2Outlined, BuildOutlined,ReceiptLongOutlined, ShoppingCartOutlined } from '@mui/icons-material'
 import { Box, useTheme, useMediaQuery } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import StatBox from 'components/StatBox'
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import HandshakeIcon from '@mui/icons-material/Handshake';
 ///tableheaders
 import { Businesscolumns, suppliercolumns, Categorycolumns } from 'utils/tableheaders'
 
@@ -16,12 +18,15 @@ const Dashboard = () => {
   const { data: clientsdata } = useGetClientsQuery();
   const { data: contractorsdata } = useGetContractorsQuery();
   const { data: categorydata, isLoading: categoryLoadingData } = useGetCategoryQuery();
+  const { data: dealsData, isLoading: isLoadingDeals } = useGetDealsQuery();
 
   //length
   const supplierlength = suplierdata?.data?.length
   const businesslength = businessdata?.data?.length
-  const clientslength = clientsdata?.length
+  const clientslength = clientsdata?.employers?.length
   const contractorslength = contractorsdata?.data?.length
+  const categorydatalength=categorydata?.length
+   const deallength = dealsData?.deals?.length
 
   const isNonMediumScreens = useMediaQuery("(min-width:1200px)");
   const theme = useTheme();
@@ -77,53 +82,7 @@ const Dashboard = () => {
             />
           }
         />
-        <Box
-          gridColumn="span 8"
-          gridRow="span 2"
-          backgroundColor={theme.palette.background.alt}
-          p="1rem"
-          borderRadius="0.55rem"
-        >
-          <DataGrid
-            loading={isLoadingSupplierdata || !suplierdata}
-            getRowId={(row) => row._id}
-            rows={suplierdata?.data || []}
-            columns={suppliercolumns}
-            sx={{
-              "& .MuiDataGrid-root": {
-                border: "none",
-                borderRadius: "5rem",
-              },
-              "& .MuiDataGrid-cell": {
-                borderBottom: "none",
-              },
-              "& .MuiDataGrid-columnHeaders": {
-                backgroundColor: theme.palette.primary.main,
-                color: theme.palette.primary.contrastText,
-                borderBottom: `1px solid ${theme.palette.divider}`,
-                "& .MuiDataGrid-columnHeaderTitle": {
-                  fontWeight: "bold",
-
-                },
-                padding: "10px 0",
-              },
-              "& .MuiDataGrid-columnSeparator": {
-                display: "none",
-              },
-              "& .MuiDataGrid-virtualScroller": {
-                backgroundColor: theme.palette.background.alt,
-              },
-              "& .MuiDataGrid-footerContainer": {
-                backgroundColor: theme.palette.background.alt,
-                color: theme.palette.secondary[100],
-                borderTop: "none",
-              },
-              "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-                color: `${theme.palette.secondary[200]} !important`,
-              },
-            }}
-          />
-        </Box>
+        
         <StatBox
           title="Total  Clients"
           value={clientslength}
@@ -137,11 +96,29 @@ const Dashboard = () => {
           title="Total Contractors"
           value={contractorslength}
           icon={
-            <AttachMoneyOutlined
+            <BuildOutlined
               sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
             />
           }
         />
+        <StatBox
+          title="Supplier Deals"
+          value={deallength}
+          icon={
+            <HandshakeIcon
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
+          }
+        /><StatBox
+        title="Supplier Types "
+        value={categorydatalength}
+        icon={
+          <PointOfSaleOutlined 
+            sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+          />
+        }
+      />
+        
 
         {/* ROW 2 */}
         <Box
